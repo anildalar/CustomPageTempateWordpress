47a2710593a53a0d4d2b376359a05dd3ce1cabf9
anildalar <dollorinfotech@gmail.com>
2021-08-27 16:39:17 +0530

    new

--- wp-content/themes/mytheme/contactus.php
+++ wp-content/themes/mytheme/contactus.php
@@ -5,26 +5,66 @@
 ?>
 <div class="main row">
     <section class="col-12 tbdr">
-    <?php
-        $args = array('post_type'=>'post');
-        // The Query
-        $the_query = new WP_Query( $args );
+        <form>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1">
+            </div>
+            <div class="mb-3 form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+            <?php
+                $args = array(
+                    'post_type'=>'post',
+                    'posts_per_page'   => 1
+                );
+                // The Query
+                $the_query = new WP_Query( $args );
+                
+                // The Loop
+                if ( $the_query->have_posts() ) {
+                   $i=1;
+                    while ( $the_query->have_posts() ) {
+                        echo '<tr>';
+                            $the_query->the_post();
+                            echo '<td>'.$i.'</td>';
+                            echo '<td>'.get_the_title().'</td>';
+                            echo '<td>'.get_the_content().'</td>';
+                            echo '<td>
+                                        <a href="#">EDIT</a>
+                                        <a href="#">DELETE</a>
+                                  </td>';
+                        echo '<tr>';
+                        $i++;
+                    }
+                } else {
+                    // no posts found
+                }
+                /* Restore original Post Data */
+                wp_reset_postdata();
+            ?>
+                </tr>
+            </tbody>
+        </table>
         
-        // The Loop
-        if ( $the_query->have_posts() ) {
-            echo '<ul>';
-            while ( $the_query->have_posts() ) {
-                $the_query->the_post();
-                echo 'This Sample Page';
-                //echo '<li>' . get_the_title() . '</li>';
-            }
-            echo '</ul>';
-        } else {
-            // no posts found
-        }
-        /* Restore original Post Data */
-        wp_reset_postdata();
-    ?>
     </section>
 </div>
 <?php get_footer( ); ?>
--- wp-content/themes/mytheme/header.php
+++ wp-content/themes/mytheme/header.php
@@ -11,6 +11,35 @@
     </head>
     <body <?php body_class(); ?>>
           <header>
-                <?php wp_nav_menu( array('theme_location'=>'primary_menu') ); ?>
-
+          <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Navbar</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <?php 
+                    wp_nav_menu( array(
+                        'before'=>'a',
+                        'after'=>'b',
+                        'menu_class'=>'collapse navbar-collapse',
+                        "menu_id"=>"navbarNav",
+                        'theme_location'=>'header_menu'
+                    ) ); 
+                ?>
+                
+            </div>
+            </nav>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Features</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Pricing</a>
+                        </li>
+                    </ul>
+                </div> 
           </header>
